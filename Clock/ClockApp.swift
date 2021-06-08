dd96dbe206dc62d3070294a3785617bbf5f23aed
Maksim <maxim.s.81@gmail.com>
2021-06-08 23:44:17 +0300

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Maksim Savin on 08.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
